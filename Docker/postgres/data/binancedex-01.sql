58260bece1a0d3251476496b984ed7f38b9eb4b7
Indika Piyasena <indika.piyasena@gmail.com>
2019-06-20 19:24:50 +0800

    postgres prefers not camelCase

--- Docker/postgres/data/binancedex-01.sql
+++ Docker/postgres/data/binancedex-01.sql
@@ -14,19 +14,18 @@ CREATE TABLE binance.transaction (
     symbol TEXT NOT NULL,
     price DOUBLE PRECISION NOT NULL,
     quantity DOUBLE PRECISION NOT NULL,
-    cumulateQuantity DOUBLE PRECISION NOT NULL,
+    cumulate_quantity DOUBLE PRECISION NOT NULL,
     fee TEXT NOT NULL,
-    orderCreateTime TIMESTAMP WITH TIME ZONE,
-    transactionTime TIMESTAMP WITH TIME ZONE,
+    order_create_time TIMESTAMP WITH TIME ZONE,
+    transaction_time TIMESTAMP WITH TIME ZONE,
     status TEXT NOT NULL,
-    timeInForce SMALLINT NOT NULL,
+    time_in_force SMALLINT NOT NULL,
     side SMALLINT NOT NULL,
-    transactionType SMALLINT NOT NULL,
-    tradeId TEXT NOT NULL,
-    lastExecutedPrice DOUBLE PRECISION NOT NULL,
-    lastExecutedQuantity DOUBLE PRECISION NOT NULL,
-    transaction_hash TEXT NOT NULL
-
+    transaction_type SMALLINT NOT NULL,
+    trade_id TEXT NOT NULL,
+    last_executed_price DOUBLE PRECISION NOT NULL,
+    last_executed_quantity DOUBLE PRECISION NOT NULL,
+    transaction_hash TEXT NOT NULL,
     PRIMARY KEY (id),
     UNIQUE (transaction_hash)
 )
