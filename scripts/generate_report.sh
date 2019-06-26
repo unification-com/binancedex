723bd64e4148e36f9d8c00c3e0ac1e7f7ea921a6
Indika Piyasena <indika.piyasena@gmail.com>
2019-06-26 09:30:33 +0800

    do not fail if file does not exist

--- scripts/generate_report.sh
+++ scripts/generate_report.sh
@@ -2,8 +2,8 @@
 
 docker exec binancedex /usr/local/bin/python -m binancedex.cli report
 
-rm /home/ubuntu/src/ui/leaderboard.html
-rm /home/ubuntu/src/ui/style.css
+rm -f /home/ubuntu/src/ui/leaderboard.html
+rm -f /home/ubuntu/src/ui/style.css
 
 docker cp binancedex:/reports/leaderboard.html /home/ubuntu/src/ui/leaderboard.html
 docker cp binancedex:/reports/stylesheet.css /home/ubuntu/src/ui/stylesheet.css
