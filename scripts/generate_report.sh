6878b2715e4f82a4199f289b53e336014854ea2d
Indika Piyasena <indika.piyasena@gmail.com>
2019-06-27 09:25:48 +0800

    copy the static files into www, and docker cp will overwrite

--- scripts/generate_report.sh
+++ scripts/generate_report.sh
@@ -2,8 +2,5 @@
 
 docker exec binancedex /usr/local/bin/python -m binancedex.cli report
 
-rm -f /home/deploy/src/ui/leaderboard.html
-rm -f /home/deploy/src/ui/style.css
-
-docker cp binancedex:/reports/leaderboard.html /home/deploy/src/ui/leaderboard.html
-docker cp binancedex:/reports/stylesheet.css /home/deploy/src/ui/stylesheet.css
+docker cp binancedex:/reports/leaderboard.html /www/leaderboard.html
+docker cp binancedex:/reports/stylesheet.css /www/stylesheet.css
